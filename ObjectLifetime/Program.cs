5f826c1648424f6688c6a34c8dedec6e2bb06325
ansaris <salman.ansari.usa@gmail.com>
2016-04-26 16:58:32 -0500

    Middle of class.

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -10,6 +10,67 @@ namespace ObjectLifetime
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            /*
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            */
+
+            Car myThirdCar = new Car("Jeep", "Thunder", 1969, "Yellow");
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color);
+
+            myOtherCar.Model = "Accord";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color);
+
+            //myOtherCar = null;
+            myCar = null;
+            // "deterministic finalization" approach to garbage collection
+
+            Console.WriteLine("{0} {1} {2} {3}",
+            myOtherCar.Make,
+            myOtherCar.Model,
+            myOtherCar.Year,
+            myOtherCar.Color);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car()
+        {
+            // You could load constructors from a configuration file,
+            // a database, etc. to intialize default values
+            this.Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
         }
     }
 }
